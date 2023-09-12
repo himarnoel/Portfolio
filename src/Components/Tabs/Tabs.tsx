76ed2980308730221ace02e0ac5d6f8e6194bade
Emmanuel Olaniyi <olaimarnoel@gmail.com>
2023-09-12 20:30:55 +0100

    added props

--- src/Components/Tabs/Tabs.tsx
+++ src/Components/Tabs/Tabs.tsx
@@ -6,11 +6,11 @@ type Props = {
   setCheckIndex: React.Dispatch<React.SetStateAction<number>>;
 };
 
-const Tabs: React.FC<Props> = ({ checkIndex, setCheckIndex, tabs }) => {
+const Tabs: React.FC<Props> = (prop) => {
   return (
     <div>
       <div className="w-full flex justify-around items-center h-[4rem] bg-[#D9D9D9] rounded-[10px]">
-        {tabs.map((item, index) => (
+        {prop.tabs.map((item, index) => (
           <div key={index} className="bg-[#42446E] rounded-lg">
             <p>{item}</p>
           </div>
