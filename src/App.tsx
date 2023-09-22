e6fc3e28613bd0aeedddb48d69e478a99525aa97
Emmanuel Olaniyi <olaimarnoel@gmail.com>
2023-09-22 04:42:34 +0100

    added tools

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ import Projects from "./Components/Tabs/Projects";
 function App() {
   return (
     <>
-      <div className="px-2 lg:px-20 mx-auto min-h-screen ">
+      <div className="px-4 lg:px-20 mx-auto min-h-screen ">
         <NavBar />
         <Hero />
       </div>
-      <div className="px-2 lg:px-20 mx-auto min-h-screen ">
+      <div className="px-4 lg:px-20 mx-auto min-h-screen ">
         <Projects />
       </div>
     </>
