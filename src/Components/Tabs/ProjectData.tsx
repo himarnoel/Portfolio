ae95d47bfcf2d0167676d9e4c58a98b11874d53c
Emmanuel Olaniyi <olaimarnoel@gmail.com>
2023-09-24 16:06:10 +0100

    worked on projects

--- src/Components/Tabs/ProjectData.tsx
+++ src/Components/Tabs/ProjectData.tsx
@@ -1,9 +1,11 @@
-import React from "react";
-import { useState } from "react";
+import img1 from "../../assets/images/bg1.png";
 import img2 from "../../assets/images/p2.png";
 import img3 from "../../assets/images/p3.png";
+import img4 from "../../assets/images/p4.png";
+import img5 from "../../assets/images/p5.png";
+import img6 from "../../assets/images/p6.png";
 type projectData = {
-  image: any;
+  image: string;
   title: string;
   text: string;
   previewlink: string;
@@ -13,7 +15,7 @@ type projectData = {
 const ProjectData = () => {
   let projectData: Array<projectData> = [
     {
-      image: img2,
+      image: img1,
       title: "Previous Exam Papers Hubs",
       text: "Designed for universities, this app offers students unlimited access to past exam questions. Representatives from different departments and levels upload questions with individual login details, addressing a drawback of the old system.",
       previewlink: "https://lurm.vercel.app/",
@@ -21,7 +23,7 @@ const ProjectData = () => {
       techstack: "React, Tailwind and CSS",
     },
     {
-      image: img3,
+      image: img2,
       title: "Gigamoni",
       text: "A fintech web application developed to facilitate international money transfers from Nigeria to individuals in foreign countries.",
       previewlink: "https://gigamoni.vercel.app/",
@@ -37,12 +39,28 @@ const ProjectData = () => {
       techstack: "React, Tailwind and CSS",
     },
     {
-      image: img3,
-      title: "Gigamoni Admin",
-      text: "This serves as Gigamoni's administrative portal, dedicated to managing and processing user-initiated transactions.",
-      previewlink: "https://gigamoniadmin.vercel.app/",
-      githublink: "https://github.com/himarnoel/gigamoniadmin",
-      techstack: "React, Tailwind and CSS",
+      image: img4,
+      title: "Interno Landing Page",
+      text: "A webpage designed for an interior decoration company, showcasing its range of products and services aimed at boosting sales.",
+      previewlink: "https://interior-dec.vercel.app/",
+      githublink: "https://github.com/himarnoel/interior_dec",
+      techstack: "ReactJs, Tailwind CSS",
+    },
+    {
+      image: img5,
+      title: "Colab Apes WaitList",
+      text: "A webpage designed for an interior decoration company, showcasing its range of products and services aimed at boosting sales.",
+      previewlink: "https://interior-dec.vercel.app/",
+      githublink: "https://github.com/himarnoel/interior_dec",
+      techstack: "ReactJs, Tailwind and CSS",
+    },
+    {
+      image: img6,
+      title: "Bank Application",
+      text: "A web-based banking application that possesses the functionality to execute transfers, withdrawals, deposit funds, and display transaction history.",
+      previewlink: "https://imabank.netlify.app/",
+      githublink: "https://github.com/himarnoel/Bank-App",
+      techstack: "React, Bootstrap, Material Ui and CSS",
     },
   ];
 
