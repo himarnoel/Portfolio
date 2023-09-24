37f975c7d97588820252b0492990a36339e17a4d
Emmanuel Olaniyi <olaimarnoel@gmail.com>
2023-09-24 19:21:23 +0100

    improved on lg

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -15,30 +15,31 @@ const NavBar = () => {
         />
 
         <a
-          className="md:px-8 px-4 rounded py-2 btn text-white lg:hidden"
+          className="md:px-8 px-4 rounded fixed right-4 py-2 btn text-white lg:hidden"
           download="Emmanuel Olaniyi.pdf"
           href="../../src/assets/Resume.pdf"
         >
           Resume
         </a>
 
-        <div className="hidden lg:flex gap-x-4  items-center font-medium text-[#666666] ">
+        <div className="hidden lg:flex gap-x-6 fixed z-20 right-20 items-center  font-medium text-[#666666] ">
+          <button>
+            <a
+              className="px-8 rounded py-2 btn text-white"
+              download="Emmanuel Olaniyi.pdf"
+              href="../../src/assets/Resume.pdf"
+            >
+              Resume
+            </a>
+          </button>
           <a href="https://github.com/himarnoel">
-            <AiFillGithub className="text-3xl" />
+            <AiFillGithub className="text-3xl bg-white rounded-full" />
           </a>
           <a href="https://www.linkedin.com/in/imanwel/">
-            <TiSocialLinkedinCircular className="text-4xl" />
+            <TiSocialLinkedinCircular className="text-4xl bg-white rounded-full" />
           </a>
           <a href="https://twitter.com/theMOGDev">
-            <AiFillTwitterCircle className="text-3xl" />
-          </a>
-
-          <a
-            className="px-8 rounded py-2 btn text-white"
-            download="Emmanuel Olaniyi.pdf"
-            href="../../src/assets/Resume.pdf"
-          >
-            Resume
+            <AiFillTwitterCircle className="text-3xl bg-white rounded-full" />
           </a>
         </div>
       </nav>
