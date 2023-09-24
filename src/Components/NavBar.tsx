cbad248beaa3d20dce28c99795727e65c46e4e42
Emmanuel Olaniyi <olaimarnoel@gmail.com>
2023-09-24 20:08:28 +0100

    done

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -17,7 +17,8 @@ const NavBar = () => {
         <a
           className="md:px-8 px-4 rounded fixed right-4 py-2 btn text-white lg:hidden"
           download="Emmanuel Olaniyi.pdf"
-          href="../../src/assets/Resume.pdf"
+          target="_blank"
+          href="https://drive.google.com/file/d/1LNYVoLY19qAnDThMwHZoz5EJxLkqceX_/view"
         >
           Resume
         </a>
@@ -34,15 +35,6 @@ const NavBar = () => {
         </div>
 
         <div className="hidden lg:flex gap-x-6 fixed z-20 right-20 items-center   font-medium text-[#666666] ">
-          <button>
-            <a
-              className="px-8 rounded py-2 btn text-white"
-              download="Emmanuel Olaniyi.pdf"
-              href="../../src/assets/Resume.pdf"
-            >
-              Resume
-            </a>
-          </button>
           <a href="https://github.com/himarnoel">
             <AiFillGithub className="text-3xl bg-white rounded-full" />
           </a>
@@ -51,6 +43,15 @@ const NavBar = () => {
           </a>
           <a href="https://twitter.com/theMOGDev">
             <AiFillTwitterCircle className="text-3xl bg-white rounded-full" />
+          </a>
+
+          <a
+            className="px-8 rounded py-2 btn text-white"
+            download="Emmanuel Olaniyi.pdf"
+            target="_blank"
+            href="https://drive.google.com/file/d/1LNYVoLY19qAnDThMwHZoz5EJxLkqceX_/view"
+          >
+            Resume
           </a>
         </div>
       </nav>
